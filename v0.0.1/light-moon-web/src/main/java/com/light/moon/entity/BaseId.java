f8e6ca7b84c14adf5d9394a6ab0e304fdffcdd79
haimaochong <292071141@qq.com>
2017-08-11 17:15:03 +0800

    修改项目配置

--- v0.0.1/light-moon-web/src/main/java/com/light/moon/entity/BaseId.java
+++ v0.0.1/light-moon-web/src/main/java/com/light/moon/entity/BaseId.java
@@ -4,15 +4,15 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
-import org.hibernate.annotations.GenericGenerator;
 
 /**
- * 实体积累
+ * 实体基类
  * 
  * @author lihh
  * 
@@ -26,12 +26,11 @@ public class BaseId implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = -3806911249585431287L;
-	
+
 	protected Long id;// 实体主键
 
 	@Id
-	@GeneratedValue(generator = "localIdGenerator")
-	@GenericGenerator(name = "localIdGenerator", strategy = "com.light.moon.entity.LocalIdGenerator")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "ID")
 	public Long getId() {
 		return id;
